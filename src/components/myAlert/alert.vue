35cd348796e7eae961b58b95f1f1b96868311641
btbrad <biantaobrad@gmail.com>
2020-08-01 22:42:20 +0800

    弹窗样式

--- src/components/myAlert/alert.vue
+++ src/components/myAlert/alert.vue
@@ -2,7 +2,7 @@
  * @Author: btbrad
  * @Date: 2020-07-31 23:45:58
  * @LastEditors: btbrad
- * @LastEditTime: 2020-08-01 16:46:21
+ * @LastEditTime: 2020-08-01 22:30:08
  * @Description:
 -->
 <template>
@@ -16,14 +16,48 @@ export default {
   name: 'Alert',
   data () {
     return {
-      content: 'this is Alert'
+      content: 'This Is Alert'
     }
   },
+  mounted () {
+    this.timer = setTimeout(() => {
+      this.$destroy()
+      document.body.removeChild(this.$el)
+      console.log(1111111111111111)
+    }, 3000)
+  },
   methods: {
-
+    // show () {
+    //   console.log(11111111111111111111111)
+    //   this.isShow = true
+    //   setTimeout(() => {
+    //     this.hide()
+    //   }, this.duration)
+    // },
+    // hide () {
+    //   this.isShow = false
+    // }
+  },
+  beforeDestroy () {
+    this.timer = null
   }
 }
 </script>
 
-<style>
+<style scoped>
+.alert-wrapper {
+  width: 300px;
+  min-height: 50px;
+  border: 1px solid #333;
+  position: absolute;
+  top: 30px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background: #fff;
+  padding: 20px;
+  box-sizing: border-box;
+  text-align: center;
+  font-weight: 700;
+  box-shadow: 0 5px 5px #ccc;
+}
 </style>
