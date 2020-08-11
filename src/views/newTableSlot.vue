70d2e5e6c9abea9a3a875af22aa0a1455a15e0d7
bt <biantaobrad@gmail.com>
2020-08-11 11:13:33 +0800

    布局完成

--- src/views/newTableSlot.vue
+++ src/views/newTableSlot.vue
@@ -2,8 +2,26 @@
   <div>
     <h2>slot实现table自定义列</h2>
     <my-table :tableData="tableData" :headItems="headItems">
-      <template v-slot:age="{ row }">
-        <span>年龄{{row}}</span>
+      <template v-slot:name="{ row, index }">
+        <input v-if="editIndex === index" type="text" v-model="row.name">
+        <span v-else>{{row.name}}</span>
+      </template>
+      <template v-slot:age="{ row, index }">
+        <input v-if="editIndex === index" type="text" v-model="row.age">
+        <span v-else>{{row.age}}</span>
+      </template>
+      <template v-slot:gender="{ row, index }">
+        <input v-if="editIndex === index" type="text" v-model="row.gender">
+        <span v-else>{{row.gender}}</span>
+      </template>
+      <template v-slot:oprate="{ row, index }">
+        <div v-if="editIndex === index">
+          <button @click="handleConfirm">确定</button>
+          <button @click="handleCancel">取消</button>
+        </div>
+        <span v-else>
+          <button @click="handleEdit(index)">编辑</button>
+        </span>
       </template>
     </my-table>
   </div>
@@ -19,6 +37,7 @@ export default {
   },
   data () {
     return {
+      editIndex: -1,
       tableData: [
         { name: 'Michael', age: '29', gender: 'male' },
         { name: 'Jane', age: '23', gender: 'female' },
@@ -27,8 +46,20 @@ export default {
       headItems: [
         { prop: 'name', label: '姓名' },
         { prop: 'age', label: '年龄', slot: true },
-        { prop: 'gender', label: '性别' }
+        { prop: 'gender', label: '性别' },
+        { prop: 'oprate', label: '操作', slot: true }
       ]
+    }
+  },
+  methods: {
+    handleEdit (index) {
+      this.editIndex = index
+    },
+    handleConfirm () {
+
+    },
+    handleCancel () {
+      this.editIndex = -1
     }
   }
 }
