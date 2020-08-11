b76e7f4cbbf70eeeba72c8dc0b8b52ad2a21b1c5
bt <biantaobrad@gmail.com>
2020-08-11 11:25:22 +0800

    v-slot改为简写#

--- src/views/newTableSlot.vue
+++ src/views/newTableSlot.vue
@@ -2,19 +2,19 @@
   <div>
     <h2>slot实现table自定义列</h2>
     <my-table :tableData="tableData" :headItems="headItems">
-      <template v-slot:name="{ row, index }">
+      <template #name="{ row, index }">
         <input v-if="editIndex === index" type="text" v-model="row.name">
         <span v-else>{{row.name}}</span>
       </template>
-      <template v-slot:age="{ row, index }">
+      <template #age="{ row, index }">
         <input v-if="editIndex === index" type="text" v-model="row.age">
         <span v-else>{{row.age}}</span>
       </template>
-      <template v-slot:gender="{ row, index }">
+      <template #gender="{ row, index }">
         <input v-if="editIndex === index" type="text" v-model="row.gender">
         <span v-else>{{row.gender}}</span>
       </template>
-      <template v-slot:oprate="{ row, index }">
+      <template #oprate="{ row, index }">
         <div v-if="editIndex === index">
           <button @click="handleConfirm">确定</button>
           <button @click="handleCancel">取消</button>
